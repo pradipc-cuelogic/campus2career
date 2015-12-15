55b719d43b7a7f7676bd2c6058e961c0f2037fa1
sagarp-cuelogic <sagar.patil@cuelogic.co.in>
2015-12-15 15:51:27 +0530

    changes updated to counselor-student-list.html

--- js/master-layout.js
+++ js/master-layout.js
@@ -110,3 +110,22 @@ function notificationToggle() {
         }
     });
 }
+
+//________________________________ Custom Checkbox ________________________________ 
+
+function customCheckbox(checkboxName){
+    var checkBox = $('input[name="'+ checkboxName +'"]');
+    $(checkBox).each(function(){
+        $(this).wrap( "<span class='custom-checkbox'></span>" );
+        if($(this).is(':checked')){
+            $(this).parent().addClass("selected");
+        }
+    });
+    $(checkBox).click(function(){
+        $(this).parent().toggleClass("selected");
+    });
+}
+$(document).ready(function (){
+    customCheckbox("confirm");
+})
+
